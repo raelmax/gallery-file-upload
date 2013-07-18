23ced83c63d4e15111ce3a6f37d0ada77b9aa4bd
Rael Max <contato@raelmax.com>
2013-07-18 11:24:29 -0300

    update setup

--- setup.py
+++ setup.py
@@ -2,25 +2,11 @@
 # -*- coding:utf-8 -*-
 from setuptools import setup
 
-classifiers = ["Development Status :: 4 - Beta",
-               "Intended Audience :: Developers",
-               "License :: OSI Approved :: MIT License",
-               "Operating System :: OS Independent",
-               "Framework :: Django",
-               'Programming Language :: Python',
-               "Programming Language :: Python :: 2.7",
-               "Operating System :: OS Independent",
-               "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-               'Topic :: Software Development :: Libraries :: Python Modules']
-
-long_description = open('README.md').read()
-
 setup(
     name='gallery-file-upload',
     version='0.1',
-    long_description=long_description,
+    description='Gallery File Upload based on jQuery File Upload integration for Opps CMS',
     classifiers=classifiers,
-    keywords='Django Gallery File Upload',
     author='Rael Max',
     author_email='contato@raelmax.com',
     download_url="https://github.com/raelmax/gallery-file-upload/tarball/master",
